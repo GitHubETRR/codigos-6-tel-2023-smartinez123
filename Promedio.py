5bcb88008bc61e18729bc106ce340218b278773f
smartinez123 <90469429+smartinez123@users.noreply.github.com>
2023-11-30 21:20:01 -0300

    Update Promedio.py

--- Promedio.py
+++ Promedio.py
@@ -1,29 +1,28 @@
-def calcular_promedio(notas):
+def calcular_promedio(notas):       #recibe la variable notas 
     if not notas:
-        return 0.0  
+        return 0.0  #retorna un float
 
-    suma_notas = sum(notas)
-    cantidad_notas = len(notas)
-    promedio = suma_notas / cantidad_notas
+    suma_notas = sum(notas)         #suma la lista
+    cantidad_notas = len(notas)     #longitud de notas
+    promedio = suma_notas / cantidad_notas 
     return promedio
 
-def main():
-    try:
-        
-        num_notas = int(input("Ingrese la cantidad de notas: "))
 
-        
-        notas = []
-        for i in range(num_notas):
-            nota = float(input(f"Ingrese la nota {i + 1}: "))
-            notas.append(nota)
+try:
+    
+    num_notas = int(input("Ingrese la cantidad de notas: "))
 
-       
-        promedio = calcular_promedio(notas)
-        print(f"El promedio de notas es: {promedio}")
+    
+    notas = [] #crea una lista
+    for i in range(num_notas):  #range te da el rango de numeros que recibe 
+        nota = float(input(f"Ingrese la nota {i + 1}: "))  #para que empieze en nota 1
+        notas.append(nota)  #agrega a la lista, a lo ultimo
 
-    except ValueError:
-        print("Error: Ingrese un número válido para la cantidad de notas y las notas.")
+    
+    promedio = calcular_promedio(notas) 
+    print(f"El promedio de notas es: {promedio}") #la f es para intercalar las variables
 
-if __name__ == "__main__":
-    main()
+except ValueError:
+    print("Error: Ingrese un número válido para la cantidad de notas y las notas.")
+
+
